daca0287e374eb4d6f714d4fba441c82d37e9b47
Shmurray12 <shanemurray2004@gmail.com>
2025-01-26 17:21:17 -0800

    fixing client-side exception

--- src/hooks/useNavObserver.tsx
+++ src/hooks/useNavObserver.tsx
@@ -6,7 +6,7 @@ import {SectionId} from '../data/data';
 export const useNavObserver = (selectors: string, handler: (section: SectionId | null) => void) => {
   useEffect(() => {
     // Get all sections
-    const headings = document.querySelectorAll('#about, #resume, #portfolio, #contact');
+    const headings = document.querySelectorAll(selectors);
     const headingsArray = Array.from(headings);
     const headerWrapper = document.getElementById(headerID);
 
