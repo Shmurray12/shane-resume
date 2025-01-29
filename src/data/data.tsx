6ed4f9756eb2c6a69d17073b15a3110b10fe021e
Shmurray12 <shanemurray2004@gmail.com>
2025-01-29 09:00:39 -0800

    fixing contact section

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -311,7 +311,7 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Feel Free to Reach Out!',
   items: [
     {
       type: ContactType.Email,
